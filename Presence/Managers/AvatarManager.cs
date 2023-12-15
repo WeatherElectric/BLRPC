79d9610bcec199818d42742bd6b3ab7b63cecaff
Mae <mae@weatherelectric.xyz>
2023-12-14 23:27:35 -0800

    Instance testing

---
+++ Presence/Managers/AvatarManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using BLRPC.Presence.Helpers;
+using BLRPC.Presence.Variables;
+using BoneLib;
+using UnityEngine;
+
+namespace BLRPC.Presence.Managers
+{
+    internal static class AvatarManager
+    {
+        public static IEnumerator Update()
+        {
+            while (LevelManager.LevelLoaded)
+            {
+                UpdateAvatar();
+                yield return new WaitForSeconds(10);
+                
+                while (!LevelManager.LevelLoaded)
+                {
+                    yield return null;
+                }
+            }
+        }
+
+        private static void UpdateAvatar()
+        {
+            var rigManager = Player.rigManager;
+            var avatar = rigManager.AvatarCrate.Crate;
+            GlobalVariables.SmallImageKey = CheckBarcode.CheckAvatar(avatar.Barcode);
+            GlobalVariables.SmallImageText = avatar.Title;
+            RpcManager.Instance.SetRpc(GlobalVariables.Details, GlobalVariables.Status, GlobalVariables.LargeImageKey, GlobalVariables.LargeImageText, GlobalVariables.SmallImageKey, GlobalVariables.SmallImageText);
+        }
+    }
+}
---
+++ Presence/Variables/GlobalVariables.cs
@@ -0,0 +1,12 @@
+﻿namespace BLRPC.Presence.Variables
+{
+    public static class GlobalVariables
+    {
+        public static string Details;
+        public static string Status;
+        public static string LargeImageKey;
+        public static string LargeImageText;
+        public static string SmallImageKey;
+        public static string SmallImageText;
+    }
+}
